597ab4715c741287a0c214e6dc79631ced001d3b
dohliam <dohliam@gmail.com>
2016-02-04 11:34:55 -0800

    update scripts to allow packaging individual templates

--- scripts/repack_otp.rb
+++ scripts/repack_otp.rb
@@ -3,9 +3,22 @@
 
 require 'fileutils'
 
+# build individual templates
+if ARGV[0]
+  templates = ARGV
+  templates.each do |d|
+    otp = File.basename(d) + ".otp"
+    `cd #{d}; zip -r ../#{otp} *`
+    puts "  * Template \"#{otp}\" built successfully"
+  end
+  exit
+end
+
+# build all templates
 dirs = Dir.glob('*').select {|f| File.directory? f}
 
 dirs.each do |d|
   otp = d + ".otp"
   `cd #{d}; zip -r ../#{otp} *`
+  puts "  * Template \"#{otp}\" built successfully"
 end
--- scripts/unpack_otp.rb
+++ scripts/unpack_otp.rb
@@ -3,10 +3,24 @@
 
 require 'fileutils'
 
+# unpack individual templates
+if ARGV[0]
+  templates = ARGV
+  templates.each do |f|
+    basename = File.basename(f, ".otp")
+    FileUtils.mkdir_p basename
+    `cd #{basename}; unzip ../#{f}`
+    puts "  * Template \"#{basename}\" unpacked successfully"
+  end
+  exit
+end
+
+# unpack all templates
 files = Dir.glob("*.otp")
 
 files.each do |f|
   basename = File.basename(f, ".otp")
   FileUtils.mkdir_p basename
   `cd #{basename}; unzip ../#{f}`
+  puts "  * Template \"#{basename}\" unpacked successfully"
 end
